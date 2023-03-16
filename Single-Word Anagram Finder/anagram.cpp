5235db3a9944304bf58b9188c1dde9c6616f4780
Russell Welch <rwelch@goldfingerinc.com>
2023-03-16 17:40:16 -0400

    Final submission for anagram

--- Single-Word Anagram Finder/anagram.cpp
+++ Single-Word Anagram Finder/anagram.cpp
@@ -1,43 +1,19 @@
 /*
- * Name:
- * Date Submitted:
- * Assignment Name:
+ * Name: Russell Welch
+ * Date Submitted: 03/16/2023
+ * Assignment Name: Single-Word Anagram Finder
  */
 
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
 vector<string> loadWordlist(string filename);
-
-/*Implement the following function:
-  anagram() takes a word (string) and a wordlist of all words (vector of strings)
-  and builds a dictionary/map where the key is a specific number of times each
-  letter occurs in a word and the associated value is a vector of strings
-  containing all words using those letters (anagrams).
-  The value returned is a vector of strings containing the corresponding
-  set of anagrams
-*/
 vector<string> anagram(string word, vector<string> wordlist);
-
-int main()
-{
-    vector<string> words;
-    vector<string> anagrams;
-    string inputWord;
-    words=loadWordlist("wordlist.txt");
-    cout << "Find single-word anagrams for the following word: ";
-    cin >> inputWord;
-    anagrams = anagram(inputWord, words);
-    for (int i=0; i<anagrams.size(); i++)
-    {
-        cout << anagrams[i] << endl;
-    }
-    return 0;
-}
 
 vector<string> loadWordlist(string filename)
 {
@@ -59,7 +35,23 @@ vector<string> loadWordlist(string filename)
     return words;
 }
 
-//Implement this function
-vector<string> anagram(string word, vector<string> wordlist)
-{
+// Finds anagrams of the given word within a list of words
+vector<string> anagram(string word, vector<string> wordlist) {
+    unordered_map<string, vector<string>> anagram_groups;
+    string sorted_target = word;
+    sort(sorted_target.begin(), sorted_target.end());
+
+    // Process each word in the list
+    for (const auto& word : wordlist) {
+        string sorted_word = word;
+        sort(sorted_word.begin(), sorted_word.end());
+
+        // If the sorted words match, they are anagrams
+        if (sorted_word == sorted_target) {
+            anagram_groups[sorted_target].emplace_back(word);
+        }
+    }
+
+    // Return the anagrams corresponding to the target word
+    return anagram_groups[sorted_target];
 }
---
+++ Single-Word Anagram Finder/tempCodeRunnerFile.cpp
@@ -0,0 +1,15 @@
+int main()
+{
+    vector<string> words;
+    vector<string> anagrams;
+    string inputWord;
+    words=loadWordlist("wordlist.txt");
+    cout << "Find single-word anagrams for the following word: ";
+    cin >> inputWord;
+    anagrams = anagram(inputWord, words);
+    for (int i=0; i<anagrams.size(); i++)
+    {
+        cout << anagrams[i] << endl;
+    }
+    return 0;
+}
