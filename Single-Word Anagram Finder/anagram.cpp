22aedbd5d6e3903ef394479e9f168b2d908f5ac1
Russell Welch <rwelch@goldfingerinc.com>
2023-03-16 17:07:00 -0400

    Single-Word Anagram Finder

---
+++ Single-Word Anagram Finder/anagram.cpp
@@ -0,0 +1,65 @@
+/*
+ * Name:
+ * Date Submitted:
+ * Assignment Name:
+ */
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+vector<string> loadWordlist(string filename);
+
+/*Implement the following function:
+  anagram() takes a word (string) and a wordlist of all words (vector of strings)
+  and builds a dictionary/map where the key is a specific number of times each
+  letter occurs in a word and the associated value is a vector of strings
+  containing all words using those letters (anagrams).
+  The value returned is a vector of strings containing the corresponding
+  set of anagrams
+*/
+vector<string> anagram(string word, vector<string> wordlist);
+
+int main()
+{
+    vector<string> words;
+    vector<string> anagrams;
+    string inputWord;
+    words=loadWordlist("wordlist.txt");
+    cout << "Find single-word anagrams for the following word: ";
+    cin >> inputWord;
+    anagrams = anagram(inputWord, words);
+    for (int i=0; i<anagrams.size(); i++)
+    {
+        cout << anagrams[i] << endl;
+    }
+    return 0;
+}
+
+vector<string> loadWordlist(string filename)
+{
+    vector<string> words;
+    ifstream inFile;
+    string word;
+    inFile.open(filename);
+    if(inFile.is_open())
+    {
+        while(getline(inFile,word))
+        {
+            if(word.length() > 0)
+            {
+                words.push_back(word);
+            }
+        }
+        inFile.close();
+    }
+    return words;
+}
+
+//Implement this function
+vector<string> anagram(string word, vector<string> wordlist)
+{
+}
